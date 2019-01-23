f620afa4235b3ecba939786d79b28218b62a86b6
Glenn-David Daniel <d2rd@outlook.com>
2019-01-22 18:44:37 -0800

    solutionCode branch save

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
 const express = require('express');
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
 
-const db = require('./database/dbConfig.js');
+const db = require('./database/dbHelpers.js');
 
 const server = express();
+const PORT = process.env.PORT || 3310;
 
 server.use(express.json());
 server.use(cors());
 
 server.get('/', (req, res) => {
   res.send('Its Alive!');
+});
+
+server.post('/api/register', (req, res) => {
+  const user = req.body;
+  user.password = bcrypt.hashSync(user.password, 16);
+  db.insert(user)
+  .then(ids => {
+    res.status(201).json({id: ids[0]});
+  })
+  .catch(err => {
+    res.status(500).send(err);
+  });
+});
+
+server.post('/api/login', (req, res) => {
+  // check that username exists AND that passwords match
+  const bodyUser = req.body;
+  db.findByUsername(bodyUser.username)
+  .then(users => {
+      // username valid   hash from client == hash from db
+    if (users.length && bcrypt.compareSync(bodyUser.password, users[0].password)) {
+      res.json({ info: "correct"});
+    } else {
+      res.status(404).json({err: "invalid username or password"});
+    }
+  })
+  .catch(err => {
+    res.status(500).send(err);
+  });
 });
 
 // protect this route, only authenticated users should see it
@@ -22,4 +53,4 @@ server.get('/api/users', (req, res) => {
     .catch(err => res.send(err));
 });
 
-server.listen(3300, () => console.log('\nrunning on port 3300\n'));
+server.listen(3310, () => console.log('\nrunning on port 3310\n'));
